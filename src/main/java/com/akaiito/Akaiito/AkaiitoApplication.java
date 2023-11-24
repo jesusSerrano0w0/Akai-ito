334a013ed2fcab11b1a1dc984887430be949a111
jesusSerrano0w0 <jgmanriqueserrano@gmail.com>
2023-11-24 11:53:41 -0500

    first commit

---
+++ src/main/java/com/akaiito/Akaiito/AkaiitoApplication.java
@@ -0,0 +1,13 @@
+package com.akaiito.Akaiito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkaiitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkaiitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akaiito/Akaiito/AkaiitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akaiito.Akaiito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkaiitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
